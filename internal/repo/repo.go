af244d66fc00f922ef46ccce22832464fddad124
Tiago César Oliveira <4922781+tiagocesar@users.noreply.github.com>
2022-09-04 23:07:35 +0200

    Adding database connectivity to the repo constructor

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -3,6 +3,7 @@ package repo
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"github.com/google/uuid"
 
@@ -13,8 +14,19 @@ type repository struct {
 	db *sql.DB
 }
 
-func NewRepository(db *sql.DB) *repository {
-	return &repository{db: db}
+func NewRepository(user, pass, host, port, schema string) (*repository, error) {
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, pass, host, port, schema)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return &repository{db: db}, nil
 }
 
 func (r *repository) GetPartnerInfo(ctx context.Context, id uuid.UUID) (*models.Partner, error) {

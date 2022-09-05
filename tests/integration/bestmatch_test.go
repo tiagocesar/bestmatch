1c64a6df93e8dd11beb9c6baf0d15a53928be4ca
Tiago César Oliveira <4922781+tiagocesar@users.noreply.github.com>
2022-09-05 14:37:47 +0200

    Adding first integration test

--- tests/integration/bestmatch_test.go
+++ tests/integration/bestmatch_test.go
@@ -4,13 +4,15 @@ package integration
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
+	"log"
 	"testing"
 
 	_ "github.com/lib/pq"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tiagocesar/bestmatch/internal/bestmatch"
+	"github.com/tiagocesar/bestmatch/internal/models"
+	"github.com/tiagocesar/bestmatch/internal/repo"
 )
 
 func Test_GetMatches_Validations(t *testing.T) {
@@ -33,68 +35,41 @@ func Test_GetMatches_Validations(t *testing.T) {
 }
 
 func Test_GetMatches_Bestmatch(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		{name: "no matches found"},
-		{name: "one match in range"},
-		{name: "two matches, ordered by rating"},
+	r, err := repo.NewRepository("root", "password", "localhost", "5432", "bestmatch")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Establishing a connection against the (containerized) test db
-	dbContainer, db := setupTestDb()
-	defer dbContainer.Terminate(context.Background())
+	svc := bestmatch.NewService(r)
 
-	_ = db
+	t.Run("Two matches, ordered by best match", func(t *testing.T) {
+		req := getMatchRequestWithDefaults()
 
-	for _, test := range tests {
-		test := test
+		req.Materials = append(req.Materials,
+			"07cab731-d981-4915-9444-cc997eec351f",
+			"1606f175-3502-4028-9501-6b591c00f1f3",
+			"ac47d822-ffc9-48b7-8492-4d49e921d4df")
 
-		t.Run(test.name, func(t *testing.T) {
+		req.Address = struct {
+			Lat  string `json:"lat"`
+			Long string `json:"long"`
+		}(struct {
+			Lat  string
+			Long string
+		}{Lat: "52.3599795", Long: "4.8851198"})
 
-		})
-	}
+		m, err := svc.GetMatches(context.Background(), req)
+		require.NoError(t, err)
+
+		matches := *m
+		require.Equal(t, 2, len(matches))
+		require.Equal(t, "b276cb54-ac52-4f8c-adb1-afce5ced67c4", matches[0].Id.String())
+	})
 }
 
-// setupTestDb spins up a container running postgres, with some data loaded, for integration testing purposes
-// it's used so there's no need to define a docker-compose file for testing purposes
-func setupTestDb() (testcontainers.Container, *sql.DB) {
-	containerReq := testcontainers.ContainerRequest{
-		Image:        "postgres:latest",
-		ExposedPorts: []string{"5432/tcp"},
-		WaitingFor:   wait.ForListeningPort("5432/tcp"),
-		Env: map[string]string{
-			"POSTGRES_DB":       "bestmatch",
-			"POSTGRES_PASSWORD": "postgres",
-			"POSTGRES_USER":     "postgres",
-		},
+func getMatchRequestWithDefaults() models.ListPartnersByMatchRequest {
+	return models.ListPartnersByMatchRequest{
+		Area:        100.0,
+		PhoneNumber: "0123456789",
 	}
-
-	dbContainer, _ := testcontainers.GenericContainer(
-		context.Background(),
-		testcontainers.GenericContainerRequest{
-			ContainerRequest: containerReq,
-			Started:          true,
-		})
-
-	err := dbContainer.CopyFileToContainer(context.Background(), "./scripts/db", "/docker-entrypoint-initdb.d/", 700)
-	if err != nil {
-		panic("Failed to copy init db file to container")
-	}
-
-	host, _ := dbContainer.Host(context.Background())
-	port, _ := dbContainer.MappedPort(context.Background(), "5432")
-
-	dbURI := fmt.Sprintf("postgres://postgres:postgres@%v:%v/bestmatch?sslmode=disable", host, port.Port())
-	db, err := sql.Open("postgres", dbURI)
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	return dbContainer, db
 }
